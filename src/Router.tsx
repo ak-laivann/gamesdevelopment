47e7c25279dfb3c36b597dcf8269205d39dd1064
ak-laivann <110388514+ak-laivann@users.noreply.github.com>
2024-08-04 01:20:30 +0530

    Reaction time tester (#3)
    * The Reset button was rendered conditionally. Fixed that! Should have thought about the edge case of that condition - Shame on me!
    
    * Reaction Time Measurement has been created. Created the Timer using NodeJs Timeout and Interval function

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { MathGame } from "./pages/MathGame.layout";
+import { ReactionTimeTestingPage } from "./pages/ReactionTimeTester.layout";
 
 export const RootRouter = () => {
   return (
     <Routes>
       <Route index element={<Navigate to={"maths"} />} />
       <Route path="maths" element={<MathGame />} />
+      <Route path="reaction" element={<ReactionTimeTestingPage />} />
     </Routes>
   );
 };
